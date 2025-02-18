50bc568991ad6c95f902bb8be13511fc2edba9c5
Nathan Martinez <nmartinez@goodleap.com>
2025-02-18 14:16:24 -0800

    main commit
    POC FOR TRUFFLEHOG FORAGER.

---
+++ hey, here is my AWS key! (this a poc for.pl
@@ -0,0 +1,7 @@
+hey, here is my AWS key! (this a poc for trufflehog forager.)
+
+[default]
+aws_access_key_id = AKIAT4GVSAXXPURNLN4F
+aws_secret_access_key = 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
+output = json
+region = us-east-2
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[default]
+aws_access_key_id = AKIAT4GVSAXXPURNLN4F
+aws_secret_access_key = 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
+output = json
+region = us-east-2
