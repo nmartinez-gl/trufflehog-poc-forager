543295ccf1c3931d7517cb04e73fa8f91aa450e2
Nathan Martinez <nmartinez@goodleap.com>
2025-02-18 14:17:38 -0800

    Update hey, here is my AWS key! (this a poc for.pl

--- hey, here is my AWS key! (this a poc for.pl
+++ hey, here is my AWS key! (this a poc for.pl
@@ -1,7 +1,1 @@
-hey, here is my AWS key! (this a poc for trufflehog forager.)
-
-[default]
-aws_access_key_id = AKIAT4GVSAXXPURNLN4F
-aws_secret_access_key = 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
-output = json
-region = us-east-2
+hey, here is my key! (this a poc for trufflehog forager.)
